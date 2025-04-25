195382304b26f4d71cc1fcdb2c2f59d5d3a02055
J.C. Jones <jc@insufficient.coffee>
2025-04-25 10:55:33 -0700

    Initial rewrite into Python

---
+++ src/cleanup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+import argparse
+import logging
+import os
+from pathlib import Path
+
+from iterfzf import iterfzf
+from sh.contrib import git
+import sh
+import rich
+
+import common
+
+PARSER = argparse.ArgumentParser(
+    description="Cleanup merged jgit branches"
+)
+PARSER.add_argument(
+    "--log-level",
+    default=logging.INFO,
+    type=lambda x: getattr(logging, x.upper()),
+    help="Configure the logging level, defaults to INFO.",
+)
+PARSER.add_argument(
+    "path",
+    default=Path.cwd(),
+    type=lambda x: Path(x),
+    help="Path to operate from"
+)
+PARSER.add_argument(
+    "--tracking",
+    default="main",
+    help="Tracking branch"
+)
+
+def branch_apparently_merged(*, tracking, branch):
+    logger = logging.getLogger(__name__)
+
+    patch_id=git('patch-id', '--stable', _in=git('diff', git('merge-base', tracking, branch).strip(), branch)).split()[0]
+    logger.debug("Calculated patch-id %s between %s..%s", patch_id, tracking, branch)
+    for rev in git('rev-list', f'{branch}..{tracking}').split():
+        other_patch_id=git('patch-id', '--stable', _in=git('diff', f'{rev}~', rev)).split()[0]
+        if patch_id == other_patch_id:
+            logger.debug("Found patch-id %s at rev %s", patch_id, rev)
+            return True
+    return False
+
+def main():
+    args = PARSER.parse_args()
+    logger = common.configure_logging(args)
+    os.chdir(args.path)
+
+    branches = common.JGitBranches(args.path)
+    for branch in branches.iter():
+        try:
+            git('rev-parse', branch)
+        except sh.ErrorReturnCode_128:
+            logger.info("%s doesn't exist", branch)
+            continue
+
+        if not branch_apparently_merged(tracking=args.tracking, branch=branch):
+            continue
+
+        logger.info("%s is apparently merged", branch)
+
+        if common.confirm(f"Delete apparently-merged branch {branch}?"):
+            logger.info("Deleting %s", branch)
+            git('branch', '-D', branch, _tty_out=True)
+            branches.remove(branch)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/common.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+from iterfzf import iterfzf
+from pathlib import Path
+import logging
+
+class NoGitRepoException(Exception):
+    pass
+
+class NotConfiguredException(Exception):
+    pass
+
+def configure_logging(args):
+    logging.basicConfig(level=args.log_level)
+    logging.getLogger("sh.command").setLevel(logging.WARNING)
+    return logging.getLogger("jgit")
+
+
+def dot_git(pos=Path.cwd()):
+    while pos.name:
+        dot_git = pos / ".git"
+        if dot_git.is_dir():
+            return dot_git
+        pos = pos.parent
+    raise NoGitRepoException("Could not find a parent directory with a .git subdir")
+
+class JGitBranches:
+    def __init__(self, path):
+        self.path = dot_git(path) / "jgit-branches"
+
+    def iter(self):
+        try:
+            contents = self.path.read_text()
+            for line in contents.splitlines():
+                if not line.startswith("#"):
+                    yield line
+        except FileNotFoundError:
+            raise NotConfiguredException("No branches are configured in %s", self.path)
+
+    def list(self):
+        return list(self.iter())
+
+    def remove(self, branch):
+        contents = self.path.read_text()
+        with self.path.open("w") as f:
+            for line in contents.splitlines():
+                if line != branch:
+                    print(line, file=f)
+
+    def append(self, branch):
+        with self.path.open("a") as f:
+            print(branch, file=f)
+
+def confirm(prompt):
+    result = iterfzf(["Yes", "No"], prompt=prompt)
+    return result == "Yes"
